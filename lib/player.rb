f719e56257d1e24aea997468096fbcc08a60daaa
Aaron Macy <aaronmacy@gmail.com>
2015-08-07 23:23:03 +0000

    in the process of rewriting player

--- lib/player.rb
+++ lib/player.rb
@@ -5,17 +5,11 @@ class Player
 
   include RanksAndSuits
 
-  attr_reader :cards, :score, :sets, :runs, :melds, :spades, :diamonds,
-    :clubs, :hearts, :ace, :two, :three, :four, :five, :six, :seven,
-    :eight, :nine, :ten, :jack, :queen, :king, :deadwood_cards, :deadwood_count
+  attr_reader :cards, :deadwood_count
 
   def initialize(cards)
-    @cards, @melds, @runs, @sets, @deadwood_cards = [], [], [], [], []
-    @score, @deadwood_count = 0, 0
-    cards.each { |card| @cards << card }
-    set_vars = Proc.new { |v| instance_variable_set("@#{v}", Array.new) }
-    RANKS.each_key(&set_vars)
-    SUITS.each(&set_vars)
+    @deadwood_count = 0
+    @cards = cards
     @has_knocked = false
   end
 
@@ -27,83 +21,50 @@ class Player
     @cards.delete_at(n)
   end
 
-  def find_sets # also sorts by rank
+  def _sorted_cards
+    sorted_cards = @cards.sort_by { |card| card.rank }
+    sorted_cards.group_by { |card| card.suit }
+  end
+
+  def _sets
+    sets = []
     RANKS.each do |rank, int|
-      matches = []
-      @cards.each do |card|
-        matches << card if card.rank == int
+      matches = @cards.select do |card|
+        card.rank == int
       end
-      if matches.length >= 3
-        instance_variable_set("@#{rank}", matches)
-        instance_variable_set(:@sets, matches)
+
+      sets << matches if matches.length >= 3
+    end
+    sets.flatten
+  end
+
+  def _runs
+    runs = {}
+    _sorted_cards.each do |suit, cards|
+      runs[suit] ||= []
+      cards.each do |card|
+        # fill in this logic
       end
     end
   end
 
-  def sort_hand
-    @cards.sort_by! { |card| card.rank }
-  end
-
-  def sort_by_suit
-    SUITS.each do |suit|
-      matches = []
-      @cards.each do |card|
-        matches << card if card.suit == suit
-      end
-      instance_variable_set("@#{suit}", matches)
-    end
-  end
-
-  def find_runs
-    suits = [@clubs, @diamonds, @hearts, @spades]
-    suits.each do |suit|
-      suit.each do |card1|
-        suit.each do |card2|
-          suit.each do |card3|
-            c1, c2, c3 = card1.rank, card2.rank, card3.rank
-            unless @runs.include?(card1)
-              if c1 == c2 + 1 && c1 == c3 + 2
-                @runs << card1
-              elsif c1 == c2 - 1 && c1 == c3 + 1
-                @runs << card1
-              elsif c1 == c2 - 1 && c1 == c3 - 2
-                @runs << card1
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-
-  def find_melds
-    add_cards = Proc.new { |card| @melds << card unless @melds.include?(card) }
-    @runs.each(&add_cards)
-    @sets.each(&add_cards)
-  end
-
-#  def find_cards_in_2_melds
-#    @sets.each do |card1|
-#      @runs.each do |card2|
-#        if card1 == card2
-#          puts "Warning: #{card1} is in in two melds"
-#        end
-#      end
-#    end
+#  def find_melds
+#    add_cards = Proc.new { |card| @melds << card unless @melds.include?(card) }
+#    @runs.each(&add_cards)
+#    @sets.each(&add_cards)
 #  end
 
-  def calc_deadwood
-    def find_deadwood_cards
-      @cards.each do |card|
-        @deadwood_cards << card unless @melds.include?(card)
-      end
-    end
-    def calc_deadwood_count
-      @deadwood_cards.each { |card| @deadwood_count += card.value }
-    end
-    find_deadwood_cards
-    calc_deadwood_count
+  def find_cards_in_2_melds
+    # figure this out
   end
+
+#  def calc_deadwood
+#    @cards.each do |card|
+#      @deadwood_cards << card unless @melds.include?(card)
+#    end
+#
+#    @deadwood_cards.each { |card| @deadwood_count += card.value }
+#  end
 
   def gin?
     @deadwood_count == 0
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,200 +1,83 @@
+require "pry"
 require "minitest/autorun"
 require_relative "../lib/player"
 
 describe Player do
   before do
     @deck = Deck.new
-    @drawn_card = @deck.remove_card
     @player = Player.new(@deck.deal_hand)
-  end
-
-  describe "#initialize" do
-    it "should set some instance variables" do
-      @player.cards.must_be_instance_of Array
-      @player.cards.length.must_equal 10
-      @player.score.must_equal 0
-      @player.deadwood_count.must_equal 0
-      @player.deadwood_cards.must_equal []
-      @player.sets.must_equal []
-      @player.seven.must_equal []
-      @player.runs.must_equal []
-      @player.spades.must_equal []
-      @player.melds.must_equal []
-    end
   end
 
   describe "#draw" do
     before do
+      @drawn_card = @deck.remove_card
       @player.draw(@drawn_card)
     end
+
     it "should add a card to the player's hand" do
       @player.cards.length.must_equal 11
       @player.cards.last.must_equal @drawn_card
       @player.cards.last.must_be_instance_of Card
     end
 
-    describe "#discard" do
-      before do
-        @player.discard(10)
-      end
+  end
 
-      it "should remove a card from the player's hand" do
-        @player.cards.length.must_equal 10
-        @player.cards.last.wont_equal @drawn_card
-        @player.cards.last.must_be_instance_of Card
+  describe "#discard" do
+    before do
+      @player.discard(10)
+    end
+
+    it "should remove a card from the player's hand" do
+      @player.cards.length.must_equal 10
+      @player.cards.wont_include @drawn_card
+    end
+  end
+
+  describe "#_sorted_cards" do
+    it "sorts cards by suit" do
+      @player._sorted_cards.each do |suit, cards_array|
+        cards_array.each do |card|
+          card.suit.must_equal suit
+        end
+      end
+    end
+
+    it "sorts suits by rank" do
+      @player._sorted_cards.each do |suit, cards_array|
+        cards_array.each do |card|
+          cards_array.first.rank.must_be :<=, card.rank
+          cards_array.last.rank.must_be :>=, card.rank
+        end
       end
     end
   end
 
-  describe "#find_sets" do
-    before do
-      @card1 = Card.new(:ace, :diamonds)
-      @card2 = Card.new(:ace, :clubs)
-      @card3 = Card.new(:ace, :spades)
-      @card4 = Card.new(:two, :clubs)
-      @card5 = Card.new(:three, :clubs)
-      @card6 = Card.new(:four, :clubs)
-      @card7 = Card.new(:seven, :clubs)
-      @card8 = Card.new(:four, :diamonds)
-      @card9 = Card.new(:five, :diamonds)
-      @cards = [@card1, @card2, @card3, @card4, @card5,
-                @card6, @card7, @card9, @card8]
-      @player2 = Player.new(@cards)
-      @player2.find_sets
+  describe "#_sets" do
+    it "finds all sets" do
+      ace_of_spades = Card.new(:ace, :spades)
+      two_of_spades = Card.new(:two, :spades)
+      five_of_hearts = Card.new(:five, :hearts)
+      five_of_spades = Card.new(:five, :spades)
+      five_of_clubs = Card.new(:five, :clubs)
+      cards = [ace_of_spades, two_of_spades, five_of_hearts, five_of_spades, five_of_clubs]
+      player = Player.new(cards)
+
+      player._sets.must_equal [five_of_hearts, five_of_spades, five_of_clubs]
     end
+  end
 
-    it "should return an array of sets" do
-      @player2.ace.must_include @card1
-      @player2.ace.must_include @card2
-      @player2.ace.must_include @card3
-      @player2.sets.must_include @card1
-      @player2.sets.must_include @card2
-      @player2.sets.must_include @card3
-      @player2.two.wont_include @card4
-      @player2.three.wont_include @card5
-      @player2.four.wont_include @card6
-      @player2.seven.wont_include @card7
-      @player2.four.wont_include @card8
-      @player2.five.wont_include @card9
-    end
+  describe "#_runs" do
+    it "finds all runs" do
+      skip
+      ace_of_spades = Card.new(:ace, :spades)
+      two_of_spades = Card.new(:two, :spades)
+      three_of_spades = Card.new(:three, :spades)
+      five_of_spades = Card.new(:five, :spades)
+      five_of_clubs = Card.new(:five, :clubs)
+      cards = [ace_of_spades, two_of_spades, three_of_spades, five_of_spades, five_of_clubs]
+      player = Player.new(cards)
 
-    describe "#sort_hand" do
-      before do
-        @player2.sort_hand
-      end
-
-      it "should order the cards by rank" do
-        @player2.cards.last.must_equal @card7
-      end
-
-      describe "#sort_be_suit" do
-        before do
-          @player2.sort_by_suit
-        end
-
-        it "should sort cards by suit" do
-          @player2.clubs.must_include @card2
-          @player2.clubs.must_include @card4
-          @player2.clubs.must_include @card5
-          @player2.clubs.must_include @card6
-          @player2.clubs.must_include @card7
-          @player2.clubs.length.must_equal 5
-          @player2.diamonds.must_include @card1
-          @player2.diamonds.must_include @card8
-          @player2.diamonds.must_include @card9
-          @player2.diamonds.length.must_equal 3
-          @player2.spades.must_include @card3
-          @player2.spades.length.must_equal 1
-          @player2.hearts.length.must_equal 0
-        end
-
-        describe "#find_runs" do
-          before do
-            @player2.find_runs
-          end
-
-          it "should return an array of runs" do
-            @player2.runs.must_include @card2
-            @player2.runs.must_include @card4
-            @player2.runs.must_include @card5
-            @player2.runs.must_include @card6
-            @player2.runs.length.must_equal 4
-          end
-
-          describe "#find_melds" do
-            before do
-              @player2.find_melds
-            end
-
-            it "should return an array of all the cards in melds" do
-              @player2.melds.must_include @card1
-              @player2.melds.must_include @card2
-              @player2.melds.must_include @card3
-              @player2.melds.must_include @card4
-              @player2.melds.must_include @card5
-              @player2.melds.must_include @card6
-              @player2.melds.length.must_equal 6
-            end
-
-#          describe "#find_cards_in_2_melds" do
-#            before do
-#              @player2.find_cards_in_2_melds
-#            end
-#
-#            it "should"
-
-            describe "#calc_deadwood" do
-              before do
-                @player2.calc_deadwood
-              end
-
-              it "should calc the total deadwood" do
-                @player2.deadwood_cards.must_include @card7
-                @player2.deadwood_cards.must_include @card8
-                @player2.deadwood_cards.must_include @card9
-                @player2.deadwood_cards.length.must_equal 3
-                @player2.deadwood_count.must_equal 16
-              end
-
-              describe "#gin?" do
-                before do
-                  @player3 = Player.new([@card1, @card2, @card3,
-                                         @card4, @card5, @card6])
-                  @player3.sort_hand
-                  @player3.sort_by_suit
-                  @player3.find_sets
-                  @player3.find_runs
-                  @player3.find_melds
-                  @player3.calc_deadwood
-                end
-
-                it "should return true when gin" do
-                  @player3.gin?.must_equal true
-                end
-
-                it "should return false otherwise" do
-                  @player2.gin?.must_equal false
-                end
-
-                describe "#can_knock?" do
-                  it "should return true when deadwood <= 10" do
-                    @player3.can_knock?.must_equal true
-                  end
-
-                  it "should return false otherwise" do
-                    @player2.can_knock?.must_equal false
-                  end
-                end
-              end
-
-                describe "#undercut" do
-                end
-
-            end
-          end
-        end
-      end
+      player._sets.must_equal [ace_of_spades, two_of_spades, three_of_spades]
     end
   end
 end
-
